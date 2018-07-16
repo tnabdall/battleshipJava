429df9e3db6aea98541165a0427c94afd60c2a25
tnabdall <41209462+tnabdall@users.noreply.github.com>
2018-07-16 15:06:27 -0600

    Can make a random board for enemy now.
    Next steps:
    GameBoard
    - A function to print enemy board
    
    RunBattleship
    - Functions to implement game logic.
    - Function to see if ship is dead //low priority
    
    Ship
    - Function to see if ship is alive //low priority

--- GameBoard.java
+++ GameBoard.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 public class GameBoard{
 
-    private Ship[] ships = new Ship[5];
+    private Ship[] ships = new Ship[5]; // Variable for ships. May implement get number of ships later to allow game to be flexible.
 
-
-
-
-    private int[][] board = new int[10][10];
-
+    private int[][] board = new int[10][10]; // Variable containing the board. 10x 10 matrix.
+	
+	
+	/**
+	Constructor for GameBoard.
+	Automatically initializes the board to a blank slate and adds default 5 ships to the game.
+	*/
     public GameBoard(){
         InitializeBoard();
         ships[0] = new Ship("Carrier",5);
@@ -17,11 +19,18 @@ public class GameBoard{
         ships[3] = new Ship("Cruiser",3);
         ships[4] = new Ship("Destroyer",2);
     }
-
+	
+	/**
+	Returns game board to player.
+	@return Returns the game board.
+	*/
     public int[][] getBoard() {
         return board;
     }
-
+	
+	/** 
+	Initializes every element in the board array to 0 to symbolize it being empty.
+	*/
     private void InitializeBoard(){
         for(int i=0; i<this.board.length;i++){
             for(int j=0; j<this.board[i].length;j++){
@@ -29,7 +38,11 @@ public class GameBoard{
             }
         }
     }
-
+	
+	/** Asks the player for coordinates in the form "A1".
+	1st part of placing a ship.
+	@ return getCoordinates Two element array where first is the board's row index and the seonc is the board's column index.
+	*/
     public int[] getCoordinates(){
         int[] coords = new int[2];
         Scanner keyboard = new Scanner(System.in);
@@ -50,18 +63,23 @@ public class GameBoard{
         return coords;
     }
 	
+	/** Makes a random board. Mostly for enemy board use.
+	*/
 	public void makeRandomBoard(){
+		// Random variables for row, column, and direction
 		Random randr = new Random();
 		Random randc = new Random();
 		Random randd = new Random();
+		// coords contains row then column
 		int[] coords = new int[2];
-		int dirn; // for random direction
+		int dirn; // for random direction, convert to string later
 		String dir ="";
-		int validPlacements = 0;
+		int validPlacements = 0; //Number of ships placed correctly
 		while (validPlacements<5){
 			coords[0] = randr.nextInt(10);
 			coords[1] = randc.nextInt(10);
 			dirn = randd.nextInt(4);
+			// Converts dirn to a direction readable by isValidPlacement function
 			switch(dirn){
 				case 0:
 					dir = "u";
@@ -76,19 +94,29 @@ public class GameBoard{
 					dir = "r";
 					break;
 			}
+			// Checks to see if the ship can be placed at the coordinates and direction.
 			if (isValidPlacement(coords,dir,ships[validPlacements])){
-				System.out.print(coords[0]);
+				/*System.out.print(coords[0]);
 				System.out.print(coords[1]);
-				System.out.print(dir);
-				validPlacements+=1;
+				System.out.println(dir);
+				*/
 				placeRandomShip(coords,dir,ships[validPlacements]);
+				validPlacements+=1;// Increase counter
+
 			}
 			
 		}
 		
 		
 	}
-
+	
+	
+	/** Checks to see if a ship can be placed given the first coordinate and direction of placement.
+	@param firstcoord An array containing the row and column of the first coordinate of the ship.
+	@param direction A string that indicates what direction to continue placing the ship.
+	@param battleship The ship being placed. Important for the ships length.
+	@return A boolean indicating whether the placement is valid.
+	*/
     public boolean isValidPlacement(int[] firstcoord, String direction, Ship battleship){
         int row = firstcoord[0];
         int col = firstcoord[1];
@@ -98,7 +126,7 @@ public class GameBoard{
         //System.out.println(length);
         boolean isValid = true;
 
-        //First check to see if last extended coordinate exists on board
+        //First check to see if the extended last coordinate exists on board
         if (direction.equals("r") && col+length>9){
             isValid = false;
         }
@@ -114,19 +142,20 @@ public class GameBoard{
 
         // Returns false here so as to not execute the rest of the function which may cause an error.
         if(!isValid){
-            System.out.print("Not a valid placement.");
+            //System.out.print("Not a valid placement.");
             return isValid;
         }
 
-        //Second check if the board already has a ship placed there, so they don't intersect.
+        //Second check if the board already has a ship placed there, so they don't overlap.
+		//right
         if(direction.equals("r")){
             for (int i = 0; i<length; i++){
-                System.out.print(this.board[row][col+i]);
                 if(this.board[row][col+i]==3){
                     isValid = false;
                 }
             }
         }
+		//left
         else if(direction.equals("l")){
             for (int i = 0; i<length; i++){
                 if(this.board[row][col-i]==3){
@@ -134,6 +163,7 @@ public class GameBoard{
                 isValid = false;
             }
         }
+		//up
         else if(direction.equals("u")){
             for (int i = 0; i<length; i++){
                 if(this.board[row-i][col]==3){
@@ -141,6 +171,7 @@ public class GameBoard{
                 }
             }
         }
+		//down
         else if(direction.equals("d")){
             for (int i = 0; i<length; i++){
                 if(this.board[row+i][col]==3){
@@ -152,38 +183,60 @@ public class GameBoard{
         return isValid;
     }
 	
+	/** Places a ship on the board without further prompt from the user.
+	@param firstcoord The row and the column to start placing the ship.
+	@param direction The direction to place the rest of the ship in.
+	@param battleship The ship being placed with a name and length
+	*/	
 	public void placeRandomShip(int[] firstcoord, String direction, Ship battleship){
-		board[firstcoord[0]][firstcoord[1]] = 3; // 3 means ship is there but hidden to enemy?
-        for (int i = 0; i<battleship.getLength()-1; i++){
-            if (direction.equalsIgnoreCase("u")){
-                board[firstcoord[0]-i-1][firstcoord[1]] = 3;
-            }
-            else if (direction.equalsIgnoreCase("d")){
-                board[firstcoord[0]+i+1][firstcoord[1]] = 3;
-            }
-            else if (direction.equalsIgnoreCase("l")){
-                board[firstcoord[0]][firstcoord[1]-i-1] = 3;
-            }
-            else if (direction.equalsIgnoreCase("r")){
-                board[firstcoord[0]][firstcoord[1]+i+1] = 3;
-            }
-        }
+		if(isValidPlacement(firstcoord,direction,battleship)){ // checks to see if placement is valid
+			board[firstcoord[0]][firstcoord[1]] = 3; // 3 means ship is there but hidden to enemy?
+			// Based on direction, places the rest of the ship by changing the elements in the board to number 3.
+			for (int i = 0; i<battleship.getLength()-1; i++){
+				if (direction.equalsIgnoreCase("u")){
+					board[firstcoord[0]-i-1][firstcoord[1]] = 3;
+				}
+				else if (direction.equalsIgnoreCase("d")){
+					board[firstcoord[0]+i+1][firstcoord[1]] = 3;
+				}
+				else if (direction.equalsIgnoreCase("l")){
+					board[firstcoord[0]][firstcoord[1]-i-1] = 3;
+				}
+				else if (direction.equalsIgnoreCase("r")){
+					board[firstcoord[0]][firstcoord[1]+i+1] = 3;
+				}
+			}
+		}
 	}
 	
+	/** Places a ship with user input. Asks user for coordinates and direction of ship.
+	@param battleship Ship being placed.
+	*/
     public void placeShip(Ship battleship){
         int[] firstcoord;
         Scanner keyboard = new Scanner(System.in);
         String direction;
+		// Outer loop to see if placement is valid.
         do {
+			// Call function to get coordinates in a valid format.
             firstcoord = getCoordinates();
+			// Inner loop to ensure that the direction matches u, r, d, or l for proper use by the program.
             do {
                 System.out.print("What direction would you like to place the rest of the ship?\n Type u,r,l,d for up, right, left, or down: ");
                 direction = keyboard.next();
                 direction = direction.trim().toLowerCase();
-            }while(!direction.matches("^(u|r|d|l)$"));
+            }while(!direction.matches("^(u|r|d|l)$")); // reg exp to ensure u, r, d, or l are entered.
+			
+			//Lets user know its not a valid placement and prints the board.
+			if(!isValidPlacement(firstcoord,direction,battleship)){
+				System.out.print("Not a valid placement."); 
+				printBoard();
+			}
         }while(!isValidPlacement(firstcoord,direction,battleship));
 
         board[firstcoord[0]][firstcoord[1]] = 3; // 3 means ship is there but hidden to enemy?
+		
+		// Sets the rest of the board to 3 according to direction of placement.
         for (int i = 0; i<battleship.getLength()-1; i++){
             if (direction.equalsIgnoreCase("u")){
                 board[firstcoord[0]-i-1][firstcoord[1]] = 3;
@@ -201,28 +254,34 @@ public class GameBoard{
         printBoard();
 
     }
-
+	
+	/** Function to place all ships with user input. No extra parameters needed. Should be called to set up a user board.
+	*/
     public void placeAllShips(){
         for (int i = 0; i<this.ships.length; i++){
             System.out.println("Please place the " + ships[i].getName() + " with length of " + ships[i].getLength() +".");
             placeShip(ships[i]);
         }
     }
-
+	
+	/** Converts the element in the board matrix to a symbol
+	@param num Symbolic number to be converted to a string.
+	@return A string to be printed out.
+	*/
     private String boardMarker(int num){
         String marker;
         switch(num){
             case 0:
-                marker = " ";
+                marker = " "; //Empty space (user) or Unknown (enemy board)
                 break;
             case 1:
-                marker = "X";
+                marker = "X"; //Miss (enemy)
                 break;
             case 2:
-                marker = "O";
+                marker = "O"; //Hit (enemy)
                 break;
             case 3:
-                marker = "+"; //just for testing
+                marker = "+"; //Own battleship (user)
                 break;
             default:
                 marker = "?";
@@ -231,7 +290,9 @@ public class GameBoard{
         return marker;
 
     }
-
+	
+	/** Prints the game board. Can be used for user or enemy boards.
+	*/
     public void printBoard(){
         System.out.println("   A B C D E F G H I J"); // Column letters
         //System.out.println(" --------------------"); // Top border
--- runBattleship.java
+++ runBattleship.java
@@ -1,7 +1,7 @@
 class runBattleship{
     public static void main(String[] args){
         runBattleship runGame = new runBattleship();
-        GameBoard p1 = runGame.setupEnemyBoard();
+        GameBoard p1 = runGame.setupGameBoard();
     }
 
     public GameBoard setupGameBoard(){
